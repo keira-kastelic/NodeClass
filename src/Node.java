919d99ec712d423ff8e1ac55968f1ef952f85ceb
keira-kastelic <79609952+keira-kastelic@users.noreply.github.com>
2022-09-28 13:22:16 -0600

    Node Class Commit 4
    Adding a Driver class and adjusting mutator methods

--- src/Node.java
+++ src/Node.java
@@ -5,13 +5,13 @@ public class Node<N> {
     private N data;
 
     // constructors
-    public void Node () {} // creating an empty constructor
+    public Node () {} // creating an empty constructor
 
-    public void Node (N data){ // creating a data note constructor
+    public Node (N data){ // creating a data note constructor
         this.data = data;
     } // creating a constructor to se the node data
 
-    public void Node (Node parent, Node child, N data){ // creating a constructor to read in the variables
+    public Node (Node parent, Node child, N data){ // creating a constructor to read in the variables
         this.parent = parent;
         this.child = child;
         this.data = data;
@@ -32,12 +32,16 @@ public class Node<N> {
     }
 
     // mutators
-    public void setParent(Node parent){ // creating a mutator method for the parent variable
+    public void setParent(Node<N> parent){ // creating a mutator method for the parent variable
         this.parent = parent; //setting parent
+        if (parent.getChild() != this)
+            parent.setChild(this);
     }
 
-    public void setChild(Node child){ // creating a mutator method for the child variable
+    public void setChild(Node<N> child){ // creating a mutator method for the child variable
         this.child = child; // setting child
+        if(child.getParent() != this)
+            child.setParent(this);
     }
 
     public void setData(N data){ // creating a mutator method for the data variable
@@ -46,7 +50,7 @@ public class Node<N> {
 
     // other methods
     public String toString(){ // creating a .toString method for the node class
-        return parent + ", " + child + ", " + data; // creating the format of the printes string
+        return "Node = " + data; // creating the format of the printes string
     }
 
     public boolean equals(Node node1, Node node2){ // creating an .equals method for the node class
